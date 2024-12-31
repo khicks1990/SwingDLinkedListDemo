09cdf25f3d16c71b6ee796249f9e823224fdbe19
khicks1990 <85317318+khicks1990@users.noreply.github.com>
2024-12-31 06:36:21 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.*;
+
+/** 
+   This Swing class is used to demonstrate
+   the operations in the DLinkedList class.
+*/
+
+public class Main extends JFrame
+{    
+
+    /**
+       The main method creates an instance of the 
+       SwingDLinkedListDemo class which causes it to 
+       display its window.
+    */
+
+    public static void main(String [ ] args)
+    {
+        new SwingDLinkedListDemo();
+    }    
+}
